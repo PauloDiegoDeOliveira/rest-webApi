46bb3c16fdf11e809f4e800f343f13c17095c542
Paulo Diego <paulo.diego@adm.unip.br>
2022-03-28 14:00:29 -0300

    Teste de versionamento

---
+++ Identity.API/V2/Controllers/TesteController.cs
@@ -0,0 +1,21 @@
+﻿using Identity.API.Controllers;
+using Identity.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.API.V2.Controllers
+{
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/teste")]
+    public class TesteController : MainController
+    {
+        public TesteController(INotificador notificador, IUser appUser) : base(notificador, appUser)
+        {
+        }
+
+        [HttpGet]
+        public string Valor()
+        {
+            return "Sou a V2";
+        }
+    }
+}
